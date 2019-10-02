87ce1fe5789e0d96d90a6f700797559789535aba
Bodo Graumann <mail@bodograumann.de>
2019-10-02 10:32:17 +0200

    Workaround: Ignore the custom vue sfc block

--- config/storybook/webpack.config.js
+++ config/storybook/webpack.config.js
@@ -1,5 +1,9 @@
 module.exports = ({ config }) => {
   config.module.rules.push({
+    resourceQuery: /blockType=custom/,
+    loader: require.resolve('./ignore-loader.js')
+  })
+  config.module.rules.push({
     test: /\.vue$/,
     loader: 'storybook-addon-vue-info/loader',
     enforce: 'post'
